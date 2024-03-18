67225f388f2845a267d709333634756237d33e80
Robert Glaser <robertbglaser@gmail.com>
2024-03-18 14:08:54 -0700

    coding completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [data, setData] = useState([]) 
+  const [searchInput, setSearchInput] =useState('')
+  const [dataToRender, setDataToRender]= useState([])
+
+  const fetchData= ()=>{
+  
+     fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res =>res.json())
+     .then(res =>setData(res))
+    
+  }
+
+  const resetList=()=> {
+
+     setDataToRender([])
+     setSearchInput('')
+  }
+
+  const filterInputData =(e) =>{
+    setSearchInput(e.target.value)
+    
+  const filtered = data.filter(user => user.username.includes(searchInput));
+    
+  if (filtered.length > 0){
+    setDataToRender(filtered)
+  }
+}
+
+  useEffect(() =>{
+    fetchData()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<label>Search
+        <input type="text" onChange={filterInputData} value={searchInput}/> 
+      </label>
+    
+
+      <table>
+        <thead> 
+        <tr>
+        <th>Name</th>
+        <th>User Name</th>
+        <th>Email</th>
+        </tr>
+        </thead>
+        <tbody> 
+          {dataToRender.length > 0  ? dataToRender.map((x) =>{
+            return(
+              <tr >
+                <td key={x.name}>{x.name}</td>
+                <td key={x.username}>{x.username}</td>
+                <td key={x.email}>{x.email}</td>
+              </tr>
+            )
+          }) :
+          data.map((item) =>{
+            return(
+              <tr >
+                <td key={item.name}>{item.name}</td>
+                <td key={item.username}>{item.username}</td>
+                <td key={item.email}>{item.email}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+      <button onClick={resetList}>Reset List</button>
     </div>
   );
 }
